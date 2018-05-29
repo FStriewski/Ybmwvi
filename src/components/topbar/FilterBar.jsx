ec26b333c4df48d74e27de03de9287049b42bbf1
FStriewski <fstriewski@outlook.com>
2018-05-29 12:17:12 +0200

    Add FilterBar

---
+++ src/components/topbar/FilterBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../../styles/topBarUnit.css'
+
+
+
+export default class FilterBar extends React.Component {
+
+    render() {
+        return (
+
+            <div className="filterBarContainer">
+                Some filters
+            </div>
+        )
+    }
+
+}
