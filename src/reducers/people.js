29c1dfbc7ebb51c7b6edcab22ef2d678113f7609
FStriewski <fstriewski@outlook.com>
2018-05-29 18:13:16 +0200

    Implement Reducer

--- src/reducers/people.js
+++ src/reducers/people.js
@@ -9,7 +9,11 @@ export default function (state = [], action) {
         case FETCH_ALL_PEOPLE:
             return action.payload
         case FETCH_MORE_PEOPLE:
-            return action.payload
+            return {
+                ...state,
+                results: [...state.results, ...action.payload.results],
+                next: action.payload.next
+            };
         case FETCH_CHARACTER:
             return action.payload
         default: return state
