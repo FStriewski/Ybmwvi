4d548da2eda37a73fb84ac723dc3a5759b50f651
FStriewski <fstriewski@outlook.com>
2018-05-29 13:38:49 +0200

    Populate filterBar with some basic inputs

--- src/components/topbar/FilterBar.jsx
+++ src/components/topbar/FilterBar.jsx
@@ -5,11 +5,32 @@ import '../../styles/topBarUnit.css'
 
 export default class FilterBar extends React.Component {
 
+    handleChange = () => { }
+
+    handleClick = () => { }
+
     render() {
         return (
 
             <div className="filterBarContainer">
-                Some filters
+                <div className="textFilter">
+                    <input type="text" onChange={this.handleChange} />
+                </div>
+                <div className="genderFilter">
+                    <select name="genderSelect" onChange={this.handleChange}>
+                        <option value="all">All</option>
+                        <option value="alien">Alien</option>
+                        <option value="female" >Female</option>
+                        <option value="male">Male</option>
+                    </select>
+                </div>
+                <div className="speciesFilter">
+                    <button type="text" onClick={this.handleClick} > Species </button>
+
+                </div>
+                <div className="planetFilter">
+                    <button type="text" onClick={this.handleClick} > Planets </button>
+                </div>
             </div>
         )
     }
