8076461f10c8ce2f05272acadaf6f99c33cded14
FStriewski <fstriewski@outlook.com>
2018-05-29 14:02:45 +0200

    Add react-router dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import TopBar from './components/topbar/TopBar'
 import FilterBar from './components/topbar/FilterBar'
 import TableHeader from './components/table/TableHeader'
