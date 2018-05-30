0ca340d0872a60d1135b3ebaf2a4fd8adf7ae151
FStriewski <fstriewski@outlook.com>
2018-05-30 17:07:47 +0200

    Final style fixes

--- src/components/table/FilterBar.jsx
+++ src/components/table/FilterBar.jsx
@@ -38,10 +38,10 @@ class FilterBar extends React.Component {
                 <div className="filterBarContainer" onClick={this.toggleVisibility}> <span id="filterButton">&#x21D5; Filter &#x21D5; </span></div>
                 <div id="wrapper">
                     <div id="filters">
-                        <div className="textFilter">
+                        <div className="textFilter"><span>Name:</span>
                             <input type="text" placeholder="search" onChange={this.handleName} />
                         </div>
-                        <div className="genderFilter">
+                        <div className="genderFilter"><span>Gender:</span>
                             <select name="genderSelect" onChange={this.handleGender}>
                                 <option value="all">All</option>
                                 <option value="female">Female</option>
@@ -49,7 +49,7 @@ class FilterBar extends React.Component {
                                 <option value="n/a">n/a</option>
                             </select>
                         </div>
-                        <div className="speciesFilter">Species:
+                        <div className="speciesFilter"><span>Species:</span>
                             <select name="genderSelect" onChange={this.handleSpecies}>
                              {speciesNames.map( s => 
                                  <option key={s} value={s}>{s}</option>
