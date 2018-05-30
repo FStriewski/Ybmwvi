08acb4b9950df5ecfc11642220e034361a1b388d
FStriewski <fstriewski@outlook.com>
2018-05-30 13:36:56 +0200

    Add PlanetContainer

---
+++ src/components/details/PlanetContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default class PlanetContainer extends React.Component {
+
+    render() {
+        return (
+            <div className="planetContainer">
+                test
+            </div>
+        )
+    }
+}
